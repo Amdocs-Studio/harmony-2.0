269210aa546825071d2e753dedb6ca2612438e86
Amit Znati <amit.znati@amdocs.com>
2026-05-18 23:34:45 +0300

    Disable useMocks in index.html and simplify FeedbackHandlerMain component

--- src/base-modules/feedback-handler/components/FeedbackHandler.main.tsx
+++ src/base-modules/feedback-handler/components/FeedbackHandler.main.tsx
@@ -1,13 +1,8 @@
 import { SpinnerWidget } from './Spinner';
 import Snackbars from './Snackbars';
 import Modals from './Modals';
-import { feedbackHandlerApi } from '@sdk';
 
 export default function FeedbackHandlerMain() {
-	const { isLoading } = feedbackHandlerApi.useLoadConfigQuery();
-	if (isLoading) {
-		return <SpinnerWidget />;
-	}
 	return (
 		<div>
 			<SpinnerWidget />
